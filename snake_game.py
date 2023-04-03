8b10ba96351cfa8741f64ac4a69cdaa8c97e3180
Jamie Zolan <jamiezolan@live.com>
2023-04-03 17:16:16 -0500

    initial commit

---
+++ snake_game.py
@@ -0,0 +1,85 @@
+import pygame
+import sys
+import random
+from pygame.locals import *
+
+# Game settings
+WIDTH = 640
+HEIGHT = 480
+GRID_SIZE = 20
+GRID_WIDTH = WIDTH // GRID_SIZE
+GRID_HEIGHT = HEIGHT // GRID_SIZE
+
+# Colors
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+# Snake and hamster classes
+class Snake:
+    def __init__(self):
+        self.segments = [[GRID_WIDTH // 2, GRID_HEIGHT // 2]]
+        self.direction = (1, 0)
+
+    def move(self):
+        head_x, head_y = self.segments[0]
+        dx, dy = self.direction
+        self.segments.insert(0, [head_x + dx, head_y + dy])
+
+        if self.segments[0] == hamster.position:
+            hamster.respawn(self)
+            pygame.mixer.Sound.play(squeak_sound)
+        else:
+            self.segments.pop()
+
+    def change_direction(self, new_direction):
+        if new_direction[0] * self.direction[0] + new_direction[1] * self.direction[1] == 0:
+            self.direction = new_direction
+
+class Hamster:
+    def __init__(self):
+        self.position = [0, 0]
+        self.respawn()
+
+    def respawn(self, snake=None):
+        while self.position == [0, 0] or (snake and self.position in snake.segments):
+            self.position = [random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1)]
+
+# Initialization
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Snake')
+clock = pygame.time.Clock()
+snake = Snake()
+hamster = Hamster()
+squeak_sound = pygame.mixer.Sound('squeak.wav')
+
+# Game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            pygame.quit()
+            sys.exit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                snake.change_direction((0, -1))
+            elif event.key == K_DOWN:
+                snake.change_direction((0, 1))
+            elif event.key == K_LEFT:
+                snake.change_direction((-1, 0))
+            elif event.key == K_RIGHT:
+                snake.change_direction((1, 0))
+
+    snake.move()
+    screen.fill(WHITE)
+
+    # Draw the snake
+    for segment in snake.segments:
+        pygame.draw.rect(screen, GREEN, (segment[0] * GRID_SIZE, segment[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+
+    # Draw the hamster
+    pygame.draw.rect(screen, RED, (hamster.position[0] * GRID_SIZE, hamster.position[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+
+    pygame.display.flip()
+    clock.tick(10)
